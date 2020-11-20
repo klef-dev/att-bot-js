9481716b712b626f8f64bc8929c0e96ef5911191
Abraham Ugbeshe <abrahamugbeshe@gmail.com>
2020-11-20 09:20:55 +0100

    ✨ Introduced puppeteer for automation

--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
 import express, { Application, Request, Response } from "express";
+import puppeteer from "puppeteer";
 
 const app: Application = express();
 
@@ -6,6 +7,21 @@ const port: Number = 5000 || process.env.PORT;
 
 app.get("/", (request: Request, response: Response) => {
   response.json({ msg: "Welcome to att bot" }).status(200);
+});
+
+app.get("/att", async (request: Request, response: Response) => {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto("https://att.lmu.edu.ng/log/login");
+    await page.screenshot({ path: "att-login.png" });
+
+    browser.close();
+  } catch (error) {
+    return response.json(error);
+  }
+  return response.send("Done");
 });
 
 app.listen(port, () => {
